83914a6ecce3a43b981bf7a4f97ea8624ada1137
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-30 00:26:34 +0100

    add reducer and connect reducer and state redux with react componenet

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,20 +2,16 @@ import React , { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { v4 as uuidv4 } from 'uuid';
-
-class ShoppingList extends Component{
-
-    state = {
-        items:[
-            {id:uuidv4(),name:'Eggs'},
-            {id:uuidv4(),name:'Milk'},
-            {id:uuidv4(),name:'Steak'},
-            {id:uuidv4(),name:'Water'}
-        ]
-    }    
-
+import {connect} from 'react-redux';
+import {getItem} from '../actions/itemActions';
+import PropTypes from 'prop-types';
+class ShoppingList extends Component{  
+    componentDidMount(){
+        // call when mount the page
+        this.props.getItem();
+    }
     render() {
-        const {items} = this.state;
+        const {items} = this.props.item; // to get list from prop that is state 
         return (
             <Container>
                 <Button
@@ -61,4 +57,14 @@ class ShoppingList extends Component{
 
 }
 
-export default ShoppingList;
+ShoppingList.propTypes = { // define types because redux return to prop
+    getItem : PropTypes.func.isRequired,
+    item : PropTypes.object.isRequired
+}
+const mapStateToProps=(state)=>({
+    item: state.item
+});
+//* connect react UI with redux by connect(redux-stuff)(reactComponenet-stuff)
+// redux-stuff such as : func to return state to props for connecting with react
+// second pram is actions 
+export default connect(mapStateToProps,{getItem})(ShoppingList);
