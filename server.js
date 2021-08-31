df2ec7a8df636e9ea559ee71111c56aabebc8efd
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-31 19:28:08 +0100

    add heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const items = require('./routes/api/items');
-
+const path = require('path');
 const app = express();
 
 // Body parser middleware
@@ -17,7 +17,16 @@ mongoose.connect(db)
     .catch(err=>console.log(err));
 
 app.use('/api/items',items);
-    
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
+
 const port = process.env.port || 5000;
 
 app.listen(port,()=>console.log('server started on port : ',port));
