ec62f5fc8fab308a6712d7348dbfdc3cda2fdf0c
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-09-01 20:05:06 +0100

    Learn The Mern Stack [10] - React & Redux Auth State

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 function auth(req,res,next){
     const token = req.header('x-auth-token');
     //check for token ; 401 unauthorized
-    if(!token)res.status(401).json({ msg: 'No token, authorization denied'});
+    if(!token)return res.status(401).json({ msg: 'No token, authorization denied'});
     try{
         // verify token 
         const decoded = jwt.verify(token,config.get('jwtSecret'));
